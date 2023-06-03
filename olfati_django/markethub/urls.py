00935a594a16ad065f82e9289dcf6b01f7f990b4
aidinsh7 <aidinsh3333@gmail.com>
2023-06-03 17:50:31 +0330

    update view markehub
     fix method put and delete class MarketHubView

--- olfati_django/markethub/urls.py
+++ olfati_django/markethub/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from markethub.views import MarketHubListView
+from markethub.views import MarketHubListView,MarketHubView
 
 urlpatterns = [
     path("", MarketHubListView.as_view()),
     path("<int:pk>", MarketHubListView.as_view()),
+    path('update/<int:pk>',MarketHubView.as_view()),
+    path('delete/<int:pk>',MarketHubView.as_view())
 ]
--- olfati_django/markethub/views.py
+++ olfati_django/markethub/views.py
@@ -1,7 +1,7 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from rest_framework import status
 from markethub.models import MarketHubModel
 from markethub.serializer import MarketHubSerializer
 
@@ -26,8 +26,15 @@ class MarketHubListView(APIView):
 class MarketHubView(APIView):
     permission_classes = [IsAuthenticated]
 
-    def put(self, request):
-        pass
+    def put(self, request,pk=None):
+        data = MarketHubModel.objects.get(pk=pk)
+        srz_data = MarketHubSerializer(instance=data,data=request.data,partial=True)
+        if srz_data.is_valid():
+            srz_data.save()
+            return Response(srz_data.data)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request):
-        pass
+    def delete(self, request,pk=None):
+        data = MarketHubModel.objects.get(pk=pk)
+        data.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT) 
